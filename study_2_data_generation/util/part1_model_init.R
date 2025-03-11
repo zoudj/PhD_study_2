a43b482476ded4d6a224270fe97e94987cbb3317
zoudj <zoudanjie7@gmail.com>
2025-03-10 21:45:14 -0700

    Add two utility R scripts to subfolder study_2_data_generation/util/

---
+++ study_2_data_generation/util/part1_model_init.R
@@ -0,0 +1,21 @@
+
+
+# library(R2OpenBUGS)
+
+gen_inits <- function(M = NULL, NI = NULL, NS = NULL, nchains = 3, seed = NULL) {
+  print(paste("Use seed: ", seed, sep = ""))
+  inits <- list()
+  if(M == 1){
+    # when mean of Minor Class equal to 1
+    for (i in 1:nchains){
+      inits[[i]] <- list(b = rnorm(NI, -2, 1), a = rlnorm(NI, -0.5, 0.6), t_mu = rnorm(1, 1, 0.2), theta = runif(NS, -0.5, 2.5))
+    }
+  } else if(M == 25){
+    # when mean of Minor Class equal to 2.5
+    for (i in 1:nchains){
+      inits[[i]] <- list(b = rnorm(NI, -3, 1), a = rlnorm(NI, -0.8, 0.6), t_mu = rnorm(1, 2.5, 0.2), theta = runif(NS, 1, 4))
+    }
+  }
+  
+  return(inits)
+}
---
+++ study_2_data_generation/util/part1_model_reg.R
@@ -0,0 +1,75 @@
+
+# regular IRT model for minor class (m = 1.0)
+model_reg_m1 <- function(){
+  ### constants
+  # C, NS, NI, prop, 
+  ### responses data
+  for (s in 1:NS) {
+    for (i in 1:NI) {
+      r[s, i] <- resp[s, i]
+    }
+  }
+  ### Priors
+  t_mu ~ dnorm(1.0, 25)
+  # simulee's ability
+  for (s in 1:NS) {
+    theta[s] ~ dnorm(t_mu, 1.0)
+  }
+  theta_mean <- mean(theta[1:NS])
+  # item parameters
+  # a: discrimination; b: difficulty
+  for(i in 1:NI){
+    # discrimination parameter a
+    # apply item centering constraint
+    # b parameter true value: mu == -2, sd == 1
+    b[i] ~ dnorm(-2.0, 1)
+    # log-normal distribution mean-log = -0.5, sd-log = 0.6 (2.78 = 1/(0.6^2))
+    a[i] ~ dlnorm(-0.5, 2.78)
+  }
+  # Likelihood
+  for (s in 1:NS){
+    for (i in 1:NI){
+      # Rasch model, a == 1
+      logit(p[s,i]) <- a[i] * (theta[s] - b[i])
+      r[s,i] ~ dbern(p[s,i])
+    }
+  }
+}
+
+
+# regular IRT model for minor class (m = 2.5)
+model_reg_m25 <- function(){
+  ### constants
+  # C, NS, NI, prop, 
+  ### responses data
+  for (s in 1:NS) {
+    for (i in 1:NI) {
+      r[s, i] <- resp[s, i]
+    }
+  }
+  ### Priors
+  t_mu ~ dnorm(2.5, 25)
+  # simulee's ability
+  for (s in 1:NS) {
+    theta[s] ~ dnorm(t_mu, 1.0)
+  }
+  theta_mean <- mean(theta[1:NS])
+  # item parameters
+  # a: discrimination; b: difficulty
+  for(i in 1:NI){
+    # discrimination parameter a
+    # apply item centering constraint
+    # b parameter true value: mu == -3, sd == 1
+    b[i] ~ dnorm(-3.0, 1)
+    # log-normal distribution mean-log = -0.8, sd-log = 0.6 (2.78 = 1/(0.6^2))
+    a[i] ~ dlnorm(-0.8, 2.78)
+  }
+  # Likelihood
+  for (s in 1:NS){
+    for (i in 1:NI){
+      # Rasch model, a == 1
+      logit(p[s,i]) <- a[i] * (theta[s] - b[i])
+      r[s,i] ~ dbern(p[s,i])
+    }
+  }
+}
